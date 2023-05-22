415e7571681a1646c710f70a062d274f6d40c66f
carlos.fiori44 <carlos.fiori.santos@gmail.com>
2023-05-22 16:32:14 -0300

    Correção versão com SQLite

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 import express from 'express'
-import UserRouter from './routes/UserRoutes.js'
+import UserRouter from './routes/UserRoute.js'
+import CarrinhoRoute from './routes/CarrinhoRoute.js'
+import ItensRoute from './routes/ItensRoute.js'
+import ProductRoute from './routes/ProductRoute.js'
+import db from './database.js'
+
+await db.sync(console.log('Banco de dados OK'))
 
 const app = express()
 
 app.use(express.json())
+app.use('/user', UserRouter)
+app.use('/carrinho', CarrinhoRoute)
+app.use('/item', ItensRoute)
+app.use('/produto', ProductRoute)
 
-app.use('/', UserRouter)
-app.listen(8080, () => {console.log('API ta online!')})
+const port = process.env.PORT || 8080
+
+app.listen(port, () => {console.log('\nAPI ta online na porta ' + port + '!')})
--- model/ItemProd.js
+++ model/ItemProd.js
@@ -1,5 +1,34 @@
-export class ItemProd{
-    constructor(id_item, quantidade, id_prod){
+import { Sequelize } from "sequelize";
+import db from "../database.js"
+import Product from "./Product.js"
+import Carrinho from "./Carrinho.js";
+
+const ItemProd = db.define('itemProd', {
+    id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    precoUnitario: {
+        type: Sequelize.DOUBLE,
+        allowNull: true
+    },
+    quantidade: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+    }
+}) 
+
+export default ItemProd
+
+//ItemProd
+ItemProd.belongsTo(Product, {
+    constraints: true,
+    foreignKey: "fkProduto"
+})
+
+/*export class ItemProd{
+    constructor(id_item, quantidade, id_prod, id_carrinho){
         this.id = id_item;
         this.quantidade = quantidade;
         this.id_prod = id_prod;
@@ -18,4 +47,4 @@ export const dbItemProd = [
     new ItemProd(5, 5, 3),
     new ItemProd(6, 6, 2),
     new ItemProd(7, 1, 1)
-]
+]*/
