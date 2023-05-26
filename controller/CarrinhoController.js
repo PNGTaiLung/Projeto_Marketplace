7a11988fb6b7356cb342c962d7bdb5237f4bdd47
Maicon-Bruno <bruno.maiconoliveira@gmail.com>
2023-05-26 00:30:36 -0300

    Inserido controller e model do carrinho

--- controller/CarrinhoController.js
+++ controller/CarrinhoController.js
@@ -30,23 +30,3 @@ class CarrinhoController{
 }
 
 export default CarrinhoController;
-
-/*import { getAll } from "../model/Carrinho.js";
-import { dbProducts } from "../model/Product.js";
-import { dbItemProd } from "../model/ItemProd.js";
-
- const dbcarrinhos = getAll()
-
-dbcarrinhos.forEach(item => {
-    for (let index = 0; index < item["products"].length; index++) {
-        item["products"][index] = [dbProducts[dbItemProd[item["products"][index]-1]["id_prod"]-1]['nome'], dbItemProd[item["products"][index]-1]["quantidade"]]
-    }
-});
-
-class CarrinhoController{ 
-    static listCarrinho(req, res){   
-        res.json(dbcarrinhos)
-    }    
-}
-
-export default CarrinhoController*/
--- model/Carrinho.js
+++ model/Carrinho.js
@@ -49,21 +49,3 @@ Product.belongsToMany(Carrinho, {
     foreignKey: 'fkProduto',
     constraint: true
 })
-
-/*export class Carrinho{
-    constructor(id_compra, products, id_user){
-        this.id = id_compra
-        this.products = products;
-        this.id_user = id_user;
-    }
-}
-
-export const getAll = () => {
-    return dbCarrinho;
-}
-
-export const dbCarrinho = [
-    new Carrinho(1, [4, 7], 1),
-    new Carrinho(2, [1, 2, 3], 2),
-    new Carrinho(3, [5, 6], 3)
-]*/
